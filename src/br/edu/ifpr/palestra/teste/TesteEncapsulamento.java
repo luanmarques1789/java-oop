3f6c1749966903c78c19d09fa80d8bc3b6188633
Luan Marques <luanmarques1789@gmail.com>
2021-08-19 17:12:28 -0300

    classe de teste

--- src/br/edu/ifpr/palestra/teste/TesteEncapsulamento.java
+++ src/br/edu/ifpr/palestra/teste/TesteEncapsulamento.java
@@ -1,7 +1,18 @@
 package br.edu.ifpr.palestra.teste;
 
+import br.edu.ifpr.palestra.modelo.Certificado;
+import br.edu.ifpr.palestra.modelo.Palestra;
 import br.edu.ifpr.palestra.modelo.Palestrante;
 
 public class TesteEncapsulamento {
-  Palestrante palestrante = new Palestrante();
+  public static void main(String[] args) {
+
+    Palestrante palestrante = new Palestrante();
+    Palestra palestra = new Palestra();
+    Certificado certificado = new Certificado();
+
+    palestrante.nome = "Ricardo";
+    certificado.livro = 500;
+    palestra.tema = "Assédio Sexual";
+  }
 }
